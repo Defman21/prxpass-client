d3e1f0c8f7e2f1088324b8efface2bb80c9e9805
Defman21 <i@defman.me>
2018-03-10 23:53:34 +0300

    Multiclient: show client ID to the user
    Signed-off-by: Defman21 <i@defman.me>

--- main.go
+++ main.go
@@ -1,56 +1,64 @@
 package main
 
 import (
-    "net"
-    "log"
-    "fmt"
-    "flag"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"regexp"
 )
 
 func main() {
-    serverAddr := flag.String("server", "localhost:30303", "ngrak instanse address")
-    proxyAddr  := flag.String("proxy", "localhost:80", "proxy requests to")
-    flag.Parse()
-    conn, err := net.Dial("tcp", *serverAddr)
-    log.Printf("Connected to %v", *serverAddr)
-    log.Printf("Proxying to %v", *proxyAddr)
+	serverAddr := flag.String("server", "localhost:30303", "ngrak instanse address")
+	proxyAddr := flag.String("proxy", "localhost:80", "proxy requests to")
+	flag.Parse()
+	conn, err := net.Dial("tcp", *serverAddr)
+	log.Printf("Connected to %v", *serverAddr)
+	log.Printf("Proxying to %v", *proxyAddr)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    reply := make([]byte, 2048)
-    for {
+	idRegex, _ := regexp.Compile("^~!@=([a-z0-9]+)=@!~$")
 
-        n, err := conn.Read(reply)
+	reply := make([]byte, 2048)
+	for {
 
-        if err != nil {
-            log.Fatal(err)
-        } else {
-            writeconn, err := net.Dial("tcp", *proxyAddr)
-            log.Printf("Proxy-pass data to %v", *proxyAddr)
-            if err != nil {
-                fmt.Println(err)
-            } else {
-                _, err = writeconn.Write(reply[:n])
-                if err != nil {
-                    fmt.Println(err)
-                } else {
-                    n, err := writeconn.Read(reply)
-                    if err != nil {
-                        fmt.Println(err)
-                    } else {
-                        data := reply[:n]
-                        log.Printf("Sending data back: %v", string(data))
-                        _, err := conn.Write(data)
-                        if err != nil {
-                            log.Fatal("zulul")
-                        }
-                    }
-                    writeconn.Close()
-                }
-            }
-        }
-    }
+		n, err := conn.Read(reply)
+
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			log.Println(string(reply[:n]))
+
+			if id := idRegex.FindSubmatch(reply[:n]); id != nil {
+				log.Printf("Your ID: %v", string(id[1]))
+				continue
+			}
+			writeconn, err := net.Dial("tcp", *proxyAddr)
+			log.Printf("Proxy-pass data to %v", *proxyAddr)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				_, err = writeconn.Write(reply[:n])
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					n, err := writeconn.Read(reply)
+					if err != nil {
+						fmt.Println(err)
+					} else {
+						data := reply[:n]
+						log.Printf("Sending data back: %v", string(data))
+						_, err := conn.Write(data)
+						if err != nil {
+							log.Fatal("zulul")
+						}
+					}
+					writeconn.Close()
+				}
+			}
+		}
+	}
 }
-
